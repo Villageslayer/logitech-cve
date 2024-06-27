83b69990d116f06a4a9f97826c129f34ad2eac91
Villageslayer <new@email>
2024-06-27 14:18:52 +0200

    Commit mroe stuff

--- Dlltest.py
+++ Dlltest.py
@@ -23,10 +23,12 @@ my_dll.press_key.argtypes = (ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_
 # Call keyboard_open
 open_result = my_dll.keyboard_open()
 print(f"keyboard_open returned: {open_result}")
-
+found_result = my_dll.found_keyboard()
+print(f"keyboard_open returned: {found_result}")
 # Example call to press_key
 # Replace these values with the actual key codes you want to send
 my_dll.press_key(0x0B, 0x08, 0x0F, 0x0F, 0x12,0)
+
 
 # Wait for a bit before closing the keyboard
 time.sleep(5)
